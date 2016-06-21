788b266bdb4a0b605a8dd492155274a8905548fb
Aaron Parecki <aaron@parecki.com>
2016-06-20 19:09:41 -0700

    reorganize file structure
    for micropub.net

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Micropub</title>
+  <style type="text/css">
+    body {
+      background-color: #fefefe;
+      font-family: "Helvetica Neue", Helvetica, sans-serif;
+      font-size: 14pt;
+    }
+    .page {
+      max-width: 500px;
+      margin: 0 auto;
+    }
+    a {
+      color: #b3885b;
+    }
+    a:hover {
+      color: #d79e61;
+    }
+    h1 {
+      text-align: center;
+    }
+    .subtitle {
+      padding: 20px 30px;
+      font-size: 16pt;
+      background-color: #e2c09c;
+      border: 1px #a36e36 solid;
+      border-radius: 4px;
+    }
+    ul.links {
+      margin: 0;
+      margin-top: 20px;
+      padding: 0;
+      list-style-type: none;
+    }
+    ul.links li {
+      margin: 0;
+      padding: 0;
+      padding-top: 16px;
+    }
+    ul.links .main {
+      text-align: center;
+      font-size: 18pt;
+      padding-top: 26px;
+      padding-bottom: 16px;
+    }
+  </style>
+</head>
+<body>
+
+<div class="page">
+  
+  <h1>Micropub</h1>
+  
+  <div class="subtitle">Micropub is an open API standard that is used to create posts on one's own domain using third-party clients.</div>
+  
+  <ul class="links">
+    <li>The Micropub specification is being developed under the <a href="https://www.w3.org/wiki/Socialwg">W3C Social Web Working Group</a>.</li>
+    <li class="main"><a href="https://www.w3.org/TR/micropub/">Latest published version</a></li>
+    <li class="main"><a href="http://micropub.net/draft/">Latest editor's draft</a></li>
+    <li>The specification was contributed to the W3C by the IndieWebCamp community. More information and history of the spec can be found on the <a href="http://indiewebcamp.com/micropub">IndieWebCamp wiki</a>.</li>
+  </ul>
+  
+</div>
+
+</body>
+</html>
